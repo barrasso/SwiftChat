bd6d63cd1631a0df354faf8e7f2ffefb14df6797
Mark Barrasso <mark.barrasso@live.com>
2015-01-20 16:26:21 -0500

    setup login/signup functions and segue to user table

--- SwiftChat/ViewController.swift
+++ SwiftChat/ViewController.swift
@@ -9,10 +9,72 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: Outlets
+    
+    @IBOutlet var usernameTextField: UITextField!
 
-    override func viewDidLoad() {
+    // MARK: Actions
+    
+    @IBAction func signIn(sender: AnyObject)
+    {
+        // try to log user in
+        PFUser.logInWithUsernameInBackground(usernameTextField.text, password:"mypass") {
+            (user: PFUser!, error: NSError!) -> Void in
+            if user != nil {
+                
+                // successful login
+                println("logged in")
+                
+                // segue to user table view controller
+                self.performSegueWithIdentifier("showUsers", sender: self)
+                
+            } else {
+                
+                // try to sign the user up
+                var user = PFUser()
+                user.username = self.usernameTextField.text
+                user.password = "mypass"
+                
+                user.signUpInBackgroundWithBlock {
+                    (succeeded: Bool!, error: NSError!) -> Void in
+                    if error == nil {
+                        
+                        // successful sign up
+                        println("signed up")
+                        
+                        // segue to user table view controller
+                        self.performSegueWithIdentifier("showUsers", sender: self)
+                        
+                    } else {
+                        
+                        // The login/signup failed
+                        NSLog("Error: %@", error)
+                    }
+                }
+            }
+        }
+    }
+    
+    // MARK: View Initialization
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    override func viewDidAppear(animated: Bool)
+    {
+        // check if current user is logged in
+        if PFUser.currentUser() != nil
+        {
+            // who is current user
+            NSLog("Current User: %@", PFUser.currentUser().username)
+            
+            // segue to user table view controller
+            self.performSegueWithIdentifier("showUsers", sender: self)
+        }
     }
 
     override func didReceiveMemoryWarning() {
